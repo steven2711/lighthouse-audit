719ca1a34f76bd7efe41977bf7326dfe520bfbc3
Steven Watson <swatson.emu@gmail.com>
2021-08-16 11:30:08 -0500

    Initial commit.

---
+++ server.js
@@ -0,0 +1,33 @@
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const express = require("express");
+const app = express();
+
+const launchChromeAndRunLighthouse = async (url) => {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const options = {
+    logLevel: "info",
+    output: "html",
+    onlyCategories: ["performance", "accessibility", "best-practices", "seo"],
+    port: chrome.port,
+  };
+
+  const runnerResult = await lighthouse(url, options);
+  const reportHtml = runnerResult.report;
+
+  await chrome.kill();
+
+  return reportHtml;
+};
+
+app.get("/", async (req, res) => {
+  const results = await launchChromeAndRunLighthouse(
+    "https://debradwinans.com/"
+  );
+
+  res.send(results);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Sever is listening on port ${PORT}`));
